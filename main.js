b7433dcac30d7ef61c8b7c36d5d6c4224bd0ffa3
boyboy1995 <kahsiak95@gmail.com>
2017-07-17 00:15:13 +0800

    conditionals & loops

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'This Is Vue Todo App',
+    someLink: 'http://www.google.com/doodle',
+    todos: [
+      { title: 'Todo list 1'},
+      { title: 'Todo list 1'},
+      { title: 'Todo list 1'},
+      { title: 'Todo list 1'},
+
+    ],
+    showTodos: true
+  }
+})
