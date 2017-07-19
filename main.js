f60de6947e722928103f814c4e93c54f8734cd4c
boyboy1995 <kahsiak95@gmail.com>
2017-07-19 17:19:46 +0800

    event handling, user inputs & delete todo list, pure increase user click count

--- main.js
+++ main.js
@@ -3,13 +3,40 @@ var app = new Vue({
   data: {
     message: 'This Is Vue Todo App',
     someLink: 'http://www.google.com/doodle',
+    todoInput: '',
     todos: [
-      { title: 'Todo list 1'},
-      { title: 'Todo list 1'},
-      { title: 'Todo list 1'},
-      { title: 'Todo list 1'},
+      { title: 'Todo  1'},
+      { title: 'Todo  2'},
+      { title: 'Todo  3'},
+      { title: 'Todo  4'},
+      { title: 'Todo  5'}
 
     ],
-    showTodos: true
+    showTodos: true,
+    count: 1
+
+  },
+  methods: {
+     createNewList: function () {
+       var todoInput = this.todoInput.trim(); // trim avoids space
+       if(todoInput){
+         this.todos.push({ title: todoInput});
+         this.todoInput = '';
+       }
+    },
+
+    clearTodo: function () {
+      this.todoInput = '';     // kinda useless, just to show esc button usage
+    },
+
+    upCount: function (){
+      this.count = this.count + 1;
+
+    },
+
+    removeTodo: function (index){
+      this.todos.splice(index, 1); //splice allows to delete element in an array
+
+    }
   }
-})
+});
